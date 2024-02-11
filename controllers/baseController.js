fd0183e9a68cd86441f9caef3f7a93bfc992d8d8
mr.beale <j.b.beale2015@gmail.com>
2024-02-11 01:08:25 -0600

    Removed default flash message

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,7 +4,6 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.Util.getNav()
-  req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav, errors: null})
   
 }
